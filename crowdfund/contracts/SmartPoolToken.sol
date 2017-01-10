8ad95636c43ef13a5125b2c1a47f9b68c659608a
tranvictor <vu.tran54@gmail.com>
2017-01-10 13:56:51 +0700

    add some conventional public fields to a token

--- crowdfund/contracts/SmartPoolToken.sol
+++ crowdfund/contracts/SmartPoolToken.sol
@@ -4,7 +4,11 @@ import "./zeppelin/token/StandardToken.sol";
 import "./Lockable.sol";
 
 contract SmartPoolToken is StandardToken, Lockable {
-    mapping(address => uint) public donationBalances;
+    string public name = "SmartPool";
+    string public symbol = "SPT";
+    uint public decimals = 1;
+
+    mapping(address => uint) public donationAmountInEther;
     mapping(uint => address) public donors;
     uint public donorCount;
     uint public totalFundRaised;
@@ -14,26 +18,26 @@ contract SmartPoolToken is StandardToken, Lockable {
     uint ETHER = 1 ether;
 
     event TokenMint(address newTokenHolder, uint tokensAmount);
-    event Donated(address from, uint amount, uint tokensAmount);
+    event Donated(address indexed from, uint amount, uint tokensAmount, uint blockNumber);
 
     function SmartPoolToken(uint preminedTokens) {
         _supply = 0;
-        _rate = 10;
+        _rate = 100;
         totalFundRaised = 0;
-        mintTokens(owner, safeMul(preminedTokens, ETHER) / _rate);
+        mintTokens(owner, safeMul(preminedTokens, ETHER / _rate));
     }
 
     function totalSupply() constant returns (uint supply) {
         return _supply;
     }
 
-    function mintTokens(address newTokenHolder, uint etherAmount) internal returns (uint tokensCreated){
-        uint tokensAmount = safeMul(_rate, etherAmount) / ETHER;
+    function mintTokens(address newTokenHolder, uint etherAmount) internal returns (uint){
+        uint tokensAmount = safeMul(_rate, etherAmount / ETHER);
 
         if (tokensAmount >= 1) {
             balances[newTokenHolder] = safeAdd(
                 balances[newTokenHolder], tokensAmount);
-            _supply += tokensAmount;
+            _supply = safeAdd(_supply, tokensAmount);
 
             TokenMint(newTokenHolder, tokensAmount);
             return tokensAmount;
@@ -45,24 +49,24 @@ contract SmartPoolToken is StandardToken, Lockable {
         uint etherAmount = msg.value;
         if (etherAmount <= 0) throw;
 
-        if (donationBalances[msg.sender] == 0) {
+        if (donationAmountInEther[msg.sender] == 0) {
             donors[donorCount] = msg.sender;
             donorCount += 1;
         }
 
-        donationBalances[msg.sender] = safeAdd(
-            donationBalances[msg.sender], etherAmount);
+        donationAmountInEther[msg.sender] = safeAdd(
+            donationAmountInEther[msg.sender], etherAmount);
         totalFundRaised = safeAdd(
             totalFundRaised, etherAmount);
         uint tokensCreated = mintTokens(msg.sender, etherAmount);
-        Donated(msg.sender, etherAmount, tokensCreated);
+        Donated(msg.sender, etherAmount, tokensCreated, block.number);
     }
 
-    function myDonation() constant returns (uint donation) {
-        return donationBalances[msg.sender];
+    function getDonationAmount() constant returns (uint donation) {
+        return donationAmountInEther[msg.sender];
     }
 
-    function myToken() constant returns (uint tokens) {
+    function getTokenBalance() constant returns (uint tokens) {
         return balances[msg.sender];
     }
 
